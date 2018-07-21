d05950b57cf11f867da6afd1218805021fa715d3
tkc <1204523482@qq.com>
2018-07-21 11:13:52 +0800

    delete other file

--- Single-CPU/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/tkc/Desktop/Single-CPU/test4_isim_beh.exe" -prj "C:/Users/tkc/Desktop/Single-CPU/test4_beh.prj" "work.test4" "work.glbl" 
